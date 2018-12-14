361e4a57c619ab46f237f7eaed8f29c4621dd960
Jesper Pedersen <jesper@yepz.dk>
2018-12-14 09:24:29 +0100

    SALG-504: Styling WIP

--- web/themes/byraad150_theme/src/app.js
+++ web/themes/byraad150_theme/src/app.js
@@ -2,18 +2,12 @@ import './scss/app.scss';
 import 'bootstrap/js/dist/carousel';
 import '../assets/js/poll-selection';
 
-
-// TODO: Check if show/hide is working
 (function ($) {
   $(function (context) {
-    $(".poll.poll-content-description").each(function() {
-      var $minHeight = 140;
-      if ( $(this).height() > $minHeight) {
-        $(this).removeClass("hideDescription");
-      }
-    });
-    $(".material__abstract.showmore").on('click', function() {
-      $(this).toggleClass("hideDescription");
+    $(".poll-content-description").each(function() {
+      $(".poll-content-expand").on('click', function() {
+        $(this).prev().toggleClass("expand");
+      });
     });
   });
 })(jQuery);
