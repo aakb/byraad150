691438571ee8fbfe579746674254063343946795
Troels Ugilt Jensen <tujensen@gmail.com>
2018-12-17 09:22:24 +0100

    SALG-530: Added poll results script to display results

--- web/themes/byraad150_theme/assets/js/poll-selection.js
+++ web/themes/byraad150_theme/assets/js/poll-selection.js
@@ -2,30 +2,57 @@
 (function ($) {
   Drupal.behaviors.pollSelection = {
     attach: function (context, settings) {
-
-      // Act on poll selection.
-      $('.js-poll-select').click(function () {
-        if ($(this).hasClass('is-locked')) {
-          // Action to take if poll is locked.
-        }
-        else {
-          // Remove all instances of is-active.
-          $('.js-poll-select').removeClass('is-active');
-
-          // Add is-active to selected element.
-          $(this).addClass('is-active');
-          // Get radio button id through matching label/Brick title.
-          var entity_id = $(this).data('entity-id');
-          var radioId = $('.js-poll .option').filter(function () {
-            return ($(this).text() == entity_id)
-          }).attr('for');
-
-          // Set checked on radio button match.
-          $('#' + radioId).prop('checked', true);
-        }
-      });
-
       $(document).ready(function () {
+        function setResults() {
+          var choiceResults = $('.js-choice-result');
+
+          choiceResults.each(function () {
+            var choiceElement = $(this);
+
+            var parentContainer = $(this).parents('.container-fluid').first();
+
+            var choiceId = choiceElement.attr('data-choice');
+//            var result = choiceElement.attr('data-result');
+            var displayResult = choiceElement.attr('data-display-result');
+
+            var jsPollSelect =  parentContainer.find('.js-poll-select[data-entity-id="' + choiceId + '"]');
+
+            var resultElement = jsPollSelect.find('.poll-result');
+
+            if (resultElement.length === 1) {
+              resultElement.text(displayResult);
+            }
+            else {
+              var element = $('<div class="poll-result"></div>').text(displayResult);
+              jsPollSelect.append(element);
+            }
+          });
+        }
+
+        setResults();
+
+        // Act on poll selection.
+        $('.js-poll-select').click(function () {
+          if ($(this).hasClass('is-locked')) {
+            // Action to take if poll is locked.
+          }
+          else {
+            // Remove all instances of is-active.
+            $('.js-poll-select').removeClass('is-active');
+
+            // Add is-active to selected element.
+            $(this).addClass('is-active');
+            // Get radio button id through matching label/Brick title.
+            var entity_id = $(this).data('entity-id');
+            var radioId = $('.js-poll .option').filter(function () {
+              return ($(this).text() == entity_id)
+            }).attr('for');
+
+            // Set checked on radio button match.
+            $('#' + radioId).prop('checked', true);
+          }
+        });
+
         // Check state of poll for the user (If we show meter template a "total" class exists)
         // This way we know that the user cast their vote already.
         if($( ".total" ).length > 0) {
